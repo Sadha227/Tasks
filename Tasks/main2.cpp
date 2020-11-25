1cdda9abf98d6c60eb1d1f2d956137f269c739c9
unknown <varvarchik3@gmail.com>
2020-11-25 17:20:25 +0200

    Tasks gitgub

---
+++ Tasks/main2.cpp
@@ -0,0 +1,123 @@
+#include <iostream>
+#include <math.h>
+#include <Windows.h>
+
+using namespace std;
+
+/*
+int maim()
+{
+	int N;
+	cin >> N;
+	if (N % 5 == 0)
+		cout << "yes" << endl;
+	else
+		cout << "no" << endl;
+	system("paust");
+	return 0;
+}
+*/ 
+
+/*
+int main()
+{
+	double x, y;
+	cout << "Input x:";
+	cin >> x;
+	cout << "Input y:";
+	cin >> y;
+	if (x >= -1 && x <= 1 && y >= -1 && y <= 1)
+		cout << "yes" << endl;
+	else
+		cout << "no" << endl;
+	system("pause");
+	return 0;
+}*/
+
+/*
+int main()
+{
+	if((x>= -3 && x<= -1 && y >= 1 && y <= 4)|| (x <=4 && x>=1 && y <= -2 && y >= 4))
+	   cout << "yes" << endl;
+	else
+		cout << "no" << endl;
+	system("pause");
+	return 0;
+}*/
+
+/*
+int main()
+{
+	double x, y, R, R1;
+	cout << "Input x:";
+	cin >> x;
+	cout << "Input y:";
+	cin >> y;
+	cout << "Input R:";
+	cin >> R;
+	cout << "Input R1:";
+	cin >> R1;
+	if ((x * x + y * y <= R * R) && (x * x + y >= R1 * R1))
+		cout << "yes" << endl;
+	else
+		cout << "no" << endl;
+	system("pause");
+	return 0;
+}*/
+
+/*
+int main()
+{
+	double x, y;
+	cout << "Input x:";
+	cin >> x;
+	cout << "Input y:";
+	cin >> y;
+	if (y <= x + 1 && y >= x -1 && y <= -x + 1 && y >= -x - 1)
+		cout << "yes" << endl;
+	else
+		cout << "no" << endl;
+	system("pause");
+	return 0;
+}
+*/
+
+/*
+int main()
+{
+	double x, y, z;
+	cout << "Input x:";
+	cin >> x;
+	cout << "Input y:";
+	cin >> y;
+	if (x < 0)
+	{
+		if (x > y)
+			z = x;
+		else
+			z = y;
+		cout << "x < 0, Xmax =" << z << endl;
+	}
+	else
+	{
+		if (x < y)
+			z = x;
+		else
+			z = y;
+		cout << "x < 0, Xmin" << z << endl;
+	}
+	system("pause");
+	return 0;
+}*/
+
+/*
+int main()
+{
+	for (int i = 1; i <= 100; i++)
+	{
+		cout << "i = " << i << endl;
+	}
+
+	system("pause");
+	return 0;
+}*/
